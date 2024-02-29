3fe8de2f832b2bd1f5efc1f0f717dc17ecb9bc62
____ Chumo Mata <273500+jmchuma@users.noreply.github.com>
2024-02-29 16:43:23 +0100

    Parse rdfind result file
    For the time being it just lists the original files and shows a count of
    how many dups exist.

--- rdfind_result_parser.py
+++ rdfind_result_parser.py
@@ -1,17 +1,67 @@
-"""
+"""Parses rdfind result file.
+
+Arguments [directory] [result_file]
+If directory is not provided it assumes the current working directory as the
+base working.
+If result_file is not provided it ask for it and if it's not provided assumes
+rdfind_result.txt in the base directory.
 """
 
+
+import os
+import sys
 
 
 def main():
     """
     """
     # if base dir is not provided assume basedir is cwd
+    if len(sys.argv) > 1:
+        BASEDIR = sys.argv[1]
+    else:
+        BASEDIR = os.getcwd()
 
-    # if ouput file not provided assume rdfind_result_parser.txt
+    print(f"BASEDIR: {BASEDIR}")
 
     # open rdfind result file passed as arg
     # if no file passed as arg ask for file
+    if len(sys.argv) > 2:
+        RDFIND_RESULTS = BASEDIR+"/"+sys.argv[2]
+    else:
+        while True:
+            try:
+                print("Enter rdfind result file.")
+                RDFIND_RESULTS = input("Leave empty for rdfind_result.txt: ").strip()
+                if len(RDFIND_RESULTS.strip()) != 0:
+                    RDFIND_RESULTS = BASEDIR+"/"+RDFIND_RESULTS
+                else:
+                    RDFIND_RESULTS = BASEDIR+"/rdfind_result.txt"
+                break
+            except EOFError:  # control-d
+                sys.exit()
+
+    print(f"RDFIND_RESULTS: {RDFIND_RESULTS}")
+
+    with open(RDFIND_RESULTS, "r") as file_in:
+        num_main = 0
+        num_dups = 0
+        for line in file_in:
+            line = line.lstrip()
+            if line.startswith("#"):
+                continue
+
+            line = line.split()
+            if line[0] == "DUPTYPE_FIRST_OCCURRENCE":
+                num_main += 1
+                if num_dups == 0:  # first line
+                    print(f"{num_main:04d}{line[7]}", end="")
+                else:
+                    print(f" :: {num_dups}\n{num_main:04d} {line[7]}", end="")
+                    num_dups = 0
+            else:
+                num_dups += 1
+        else: # EOF
+            print(f" :: {num_dups}")
 
     # if output file not provided assume rdfind_result_parser.txt
     # open file
@@ -38,7 +88,6 @@ def main():
     #         ask if trash orig (if it's just one remaining)
     #     else ask again if compare or delete or quit
     # write report of actions to file
-    pass
 
 
 if __name__ == "__main__":
